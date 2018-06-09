190ce33329ba4a62e336acd7f5c4c1104d451246
Malthe <malthedj@hotmail.com>
2018-06-09 15:07:44 +0200

    added generic repository for microsoft entity framework.

---
+++ SwagLib/SwagLib/EntityCore/GenericRepository.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SwagLib.EntityCore
+{
+    public class GenericRepository<TE> : IGenericRepository<TE> where TE : class
+    {
+        #region Setup
+
+        internal DbContext Context;
+        internal DbSet<TE> DbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            this.Context = context;
+            this.DbSet = context.Set<TE>();
+        }
+        #endregion
+
+        #region Operations
+        public virtual IEnumerable<TE> Get(Expression<Func<TE, bool>> filter = null,
+            Func<IQueryable<TE>, IOrderedQueryable<TE>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TE> query = DbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual TE GetById(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual void Insert(TE entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public virtual void DeleteById(object id)
+        {
+            TE entityToDelete = DbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TE entity)
+        {
+            if (Context.Entry(entity).State == EntityState.Detached)
+            {
+                DbSet.Attach(entity);
+            }
+            DbSet.Remove(entity);
+        }
+
+        public virtual void Update(TE entity)
+        {
+            DbSet.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        #endregion
+
+        #region Task Operations
+
+        public async Task<IEnumerable<TE>> GetAsync(Expression<Func<TE, bool>> filter = null, Func<IQueryable<TE>, IOrderedQueryable<TE>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TE> query = DbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+
+        public async Task<TE> GetByIdAsync(object id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public Task InsertTask(TE entity)
+        {
+           return new Task ( () => DbSet.Add(entity));
+        }
+
+        public async Task DeleteByIdAsync(object id)
+        {
+            TE entityToDelete = await DbSet.FindAsync(id);
+            await DeleteTask(entityToDelete);
+        }
+
+        public Task DeleteTask(TE entity)
+        {
+            
+            if (Context.Entry(entity).State == EntityState.Detached)
+            {
+                DbSet.Attach(entity);
+            }
+            return new Task( () => DbSet.Remove(entity));
+        }
+
+        public Task UpdateTask(TE entity) 
+        {
+            Task task = new Task ( () => DbSet.Attach(entity));
+            Context.Entry(entity).State = EntityState.Modified;
+            return task;
+        }
+
+        #endregion
+    }
+}
---
+++ SwagLib/SwagLib/EntityCore/IGenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SwagLib.EntityCore
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        #region Operations
+
+        //Get
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+
+        //Get By Id
+        T GetById(object id);
+
+        //insert
+        void Insert(T entity);
+
+        //delete by id
+        void DeleteById(object id);
+
+        //delete 
+        void Delete(T entity);
+
+        //update
+        void Update(T entity);
+
+        #endregion
+
+        #region Task Operations
+
+        //Get
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+
+        //Get By Id
+        Task<T> GetByIdAsync(object id);
+
+        //insert
+        Task InsertTask(T entity);
+
+        //delete by id
+        Task DeleteByIdAsync(object id);
+
+        //delete 
+        Task DeleteTask(T entity);
+
+        //update
+        Task UpdateTask(T entity);
+
+
+        #endregion
+
+    }
+}
